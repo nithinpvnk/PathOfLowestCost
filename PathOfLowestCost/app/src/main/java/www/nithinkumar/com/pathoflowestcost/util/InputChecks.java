9df3f5b9386cd03acb895d2cb03c5603be9d61d5
nithin kumar <nithin.pvnk@gmail.com>
2018-02-03 10:22:55 -0800

    Code for the UserInputCheck Testsuite to pass

--- PathOfLowestCost/app/src/main/java/www/nithinkumar/com/pathoflowestcost/util/InputChecks.java
+++ PathOfLowestCost/app/src/main/java/www/nithinkumar/com/pathoflowestcost/util/InputChecks.java
@@ -4,10 +4,33 @@ package www.nithinkumar.com.pathoflowestcost.util;
 public class InputChecks {
 
     /*
-   * This is function used to convert the given input in the form of a string into a proper 2D Matrix
-   */
+     * This is function used to convert the given input in the form of a string into a proper 2D Matrix
+    */
     public static int[][] stringToMatrix(String input) {
 
-        return new int[0][0];
+        if (input != null) {
+            String[] rows = input.split("\n");  // Split the input with respect to the new Line to separate rows
+
+            String[] firstColumn = rows[0].split("\\s+");   //Split the values in the first row with respect to space
+
+            int[][] outputMatrix = new int[rows.length][firstColumn.length];    //The matrix that needs to be produced from the given values
+
+            try {
+                for (int row = 0; row < rows.length; row++) {
+                    String[] columns = rows[row].split("\\s+");
+                    for (int column = 0; column < columns.length; column++) {
+                        if (column < outputMatrix[0].length) {
+                            outputMatrix[row][column] = Integer.valueOf(columns[column]);
+                        }
+                    }
+                }
+
+                return outputMatrix;
+            } catch (NumberFormatException nfe) {
+                return new int[0][0];
+            }
+        } else {
+            return new int[0][0];
+        }
     }
 }

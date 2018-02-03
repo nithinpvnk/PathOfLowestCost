c00f9c7b4daa81af5bb4c940eac3c7831abe6272
nithin kumar <nithin.pvnk@gmail.com>
2018-02-03 09:48:44 -0800

    Android Project

---
+++ PathOfLowestCost/app/src/main/java/www/nithinkumar/com/pathoflowestcost/LowestCostPathDisplayActivity.java
@@ -0,0 +1,13 @@
+package www.nithinkumar.com.pathoflowestcost;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LowestCostPathDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lowest_cost_path_display);
+    }
+}

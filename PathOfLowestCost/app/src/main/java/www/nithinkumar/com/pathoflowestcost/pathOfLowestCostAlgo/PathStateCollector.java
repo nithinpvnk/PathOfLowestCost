f83d6cdfcdb867f529086887bf107721e010166b
nithin kumar <nithin.pvnk@gmail.com>
2018-02-03 18:17:10 -0800

    Collection of the paths in the Given Matrix Code

--- PathOfLowestCost/app/src/main/java/www/nithinkumar/com/pathoflowestcost/pathOfLowestCostAlgo/PathStateCollector.java
+++ PathOfLowestCost/app/src/main/java/www/nithinkumar/com/pathoflowestcost/pathOfLowestCostAlgo/PathStateCollector.java
@@ -1,11 +1,20 @@
 package www.nithinkumar.com.pathoflowestcost.pathOfLowestCostAlgo;
 
+class PathStateCollector {
+    private PathState bestPath;
+    private PathStateComparator comparator;
 
-
-public class PathStateCollector {
-    public PathState getBestPath() {
+    PathStateCollector() {
+        comparator = new PathStateComparator();
     }
 
-    public void addPath(PathState expectedPath) {
+    PathState getBestPath() {
+        return bestPath;
+    }
+
+    void addPath(PathState newPath) {
+         if (comparator.compare(newPath, bestPath) < 0) {
+            bestPath = newPath;
+        }
     }
 }

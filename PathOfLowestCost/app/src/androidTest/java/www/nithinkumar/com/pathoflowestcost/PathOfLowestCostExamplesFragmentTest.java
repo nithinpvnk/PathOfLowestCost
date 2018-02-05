0b723b9d91cfce10f4f71d104a498f359cfbc48e
nithin kumar <nithin.pvnk@gmail.com>
2018-02-04 20:25:49 -0800

    Custom TestCases and Code

--- PathOfLowestCost/app/src/androidTest/java/www/nithinkumar/com/pathoflowestcost/PathOfLowestCostExamplesFragmentTest.java
+++ PathOfLowestCost/app/src/androidTest/java/www/nithinkumar/com/pathoflowestcost/PathOfLowestCostExamplesFragmentTest.java
@@ -1,8 +1,9 @@
 package www.nithinkumar.com.pathoflowestcost;
 
-import android.support.test.espresso.ViewAssertion;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.Button;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -13,10 +14,11 @@ import www.nithinkumar.com.pathoflowestcost.util.Examples;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 @RunWith(AndroidJUnit4.class)
 public class PathOfLowestCostExamplesFragmentTest {
@@ -96,33 +98,32 @@ public class PathOfLowestCostExamplesFragmentTest {
         onView(withId(R.id.grid_contents)).check(matches(withText(expectedContents)));
     }
 
+    @Test
+    public void goButtonIsDisabledByDefault() {
+        Button goButton = fragment.getView().findViewById(R.id.go_button);
+        assertThat(goButton.isEnabled(), is(false));
+    }
 
-//    @Test
-//    public void goButtonIsDisabledByDefault() {
-//        onView(withId(R.id.go_button)).check((ViewAssertion) isEnabled());
-//        //assertThat(goButton.isEnabled(), is(false));
-//    }
+    @Test
+    public void clickingAnyExampleGridButtonEnablesGoButton() {
 
-//    @Test
-//    public void clickingAnyExampleGridButtonEnablesGoButton() {
-//
-//        onView(withId(R.id.grid_3_button)).perform(click());
-//        onView(withId(R.id.go_button)).perform(click());
-//
-//        onView(withId(R.id.results_success)).check(matches(withText("No")));
-//        Button goButton = (Button) getViewFromFragment(R.id.go_button);
-//
-//        getViewFromFragment(R.id.grid_1_button).performClick();
-//        assertThat(goButton.isEnabled(), is(true));
-//        goButton.setEnabled(false);
-//
-//        getViewFromFragment(R.id.grid_2_button).performClick();
-//        assertThat(goButton.isEnabled(), is(true));
-//        goButton.setEnabled(false);
-//
-//        getViewFromFragment(R.id.grid_3_button).performClick();
-//        assertThat(goButton.isEnabled(), is(true));
-//    }
+        onView(withId(R.id.grid_3_button)).perform(click());
+        onView(withId(R.id.go_button)).perform(click());
+
+        onView(withId(R.id.results_success)).check(matches(withText("No")));
+        Button goButton = fragment.getView().findViewById(R.id.go_button);
+
+        fragment.getView().findViewById(R.id.grid_1_button).performClick();
+        assertThat(goButton.isEnabled(), is(true));
+        goButton.setEnabled(false);
+
+        fragment.getView().findViewById(R.id.grid_2_button).performClick();
+        assertThat(goButton.isEnabled(), is(true));
+        goButton.setEnabled(false);
+
+        fragment.getView().findViewById(R.id.grid_3_button).performClick();
+        assertThat(goButton.isEnabled(), is(true));
+    }
 
     @Test
     public void clickingGoAfterClickingAGridButtonDisplaysYesIfPathSuccessful() {
--- PathOfLowestCost/app/src/main/java/www/nithinkumar/com/pathoflowestcost/PathOfLowestCostCustomFragment.java
+++ PathOfLowestCost/app/src/main/java/www/nithinkumar/com/pathoflowestcost/PathOfLowestCostCustomFragment.java
@@ -1,6 +1,106 @@
 package www.nithinkumar.com.pathoflowestcost;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
+
+import www.nithinkumar.com.pathoflowestcost.pathOfLowestCostAlgo.MatrixTwoD;
+import www.nithinkumar.com.pathoflowestcost.pathOfLowestCostAlgo.MatrixVisitedPath;
+import www.nithinkumar.com.pathoflowestcost.pathOfLowestCostAlgo.PathState;
+import www.nithinkumar.com.pathoflowestcost.util.InputChecks;
 
 public class PathOfLowestCostCustomFragment extends Fragment {
+    public PathOfLowestCostCustomFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View fragmentView = inflater.inflate(R.layout.fragment_lowest_cost_custom_main, container, false);
+
+        Button goButton = fragmentView.findViewById(R.id.go_button);
+        goButton.setOnClickListener(new GoOnClickListener());
+
+        EditText customGridContents = fragmentView.findViewById(R.id.custom_grid_contents);
+        customGridContents.addTextChangedListener(new GridContentsWatcher());
+
+        return fragmentView;
+    }
+
+    private String formatPath(PathState path) {
+        StringBuilder builder = new StringBuilder();
+        List<Integer> rows = path.getRowsTraversed();
+
+        for (int i = 0; i < rows.size(); i++) {
+            builder.append(rows.get(i));
+            if (i < rows.size() - 1) {
+                builder.append("\t");
+            }
+        }
+
+        return builder.toString();
+    }
+
+    private boolean gridContentsAreValid(int[][] contents) {
+        if (contents.length < 1 || contents.length > 10 || contents[0].length < 5 || contents[0].length > 100) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void loadGrid(int[][] contents) {
+        MatrixTwoD matrixTwoD = new MatrixTwoD(contents);
+        MatrixVisitedPath visitor = new MatrixVisitedPath(matrixTwoD);
+        PathState bestPath = visitor.getBestPathForGrid();
+
+        if (bestPath.isSuccessful()) {
+            ((TextView) getView().findViewById(R.id.results_success)).setText("Yes");
+        } else {
+            ((TextView) getView().findViewById(R.id.results_success)).setText("No");
+        }
+        ((TextView) getView().findViewById(R.id.results_total_cost)).setText(Integer.toString(bestPath.getTotalCost()));
+        ((TextView) getView().findViewById(R.id.results_path_taken)).setText(formatPath(bestPath));
+        ((TextView) getView().findViewById(R.id.grid_contents)).setText(matrixTwoD.asDelimitedString("\t"));
+    }
+
+    class GridContentsWatcher implements TextWatcher {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            Button goButton =  getView().findViewById(R.id.go_button);
+            goButton.setEnabled(!s.toString().isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) { }
+    }
+
+    class GoOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            String gridString = ((EditText) getView().findViewById(R.id.custom_grid_contents)).getText().toString();
+            int[][] potentialGridContents = InputChecks.stringToMatrix(gridString);
+            if (gridContentsAreValid(potentialGridContents)) {
+                loadGrid(potentialGridContents);
+            } else {
+                new AlertDialog.Builder(getContext())
+                        .setTitle("Invalid Grid")
+                        .setMessage(R.string.invalid_grid_message)
+                        .setPositiveButton("OK", null)
+                        .show();
+            }
+        }
+    }
 }
